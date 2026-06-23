5007ed504346f37b8e339f51a098a07d1d645ef8
sarthak <sarthaktyagi1704@gmail.com>
2026-06-23 23:47:51 +0530

    updating the gitignore file and template.py

---
+++ template.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import os
+import logging
+
+directories ={
+    "backend",
+    "docs",
+    "tests"
+}
+
+files={
+    "backend/app.py",
+    "backend/parser.py",
+    "backend/metrics.py",
+    "docs/product_requirement.md",
+    "README.md"
+}
+
+for dir in directories:
+    dir_path = Path(dir)
+    dir_path.mkdir(parents=True,exist_ok=True)
+    logging.info("directory created")
+
+for file in files:
+    file_path= Path(dir)
+    file_path.parent.mkdir(parents=True,exist_ok=True)
+
+    if not file_path.exists():
+        file_path.touch()
+        logging.info(f"Created file: {file_path}")
+    else:
+        logging.info(f"File already exists: {file_path}") 
